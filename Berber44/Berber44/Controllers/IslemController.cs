b8b5955a1d8bb19d54392cf697cbd30e5b75e26b
Göktuğ Yüceer <116160230+goktugycr@users.noreply.github.com>
2024-12-09 14:08:26 +0300

    İşlem düzenleme ve silme özellikleri eklendi.
    Islem sayfasına işlem düzenleme ve işlem silme özellikleri eklendi.

--- Berber44/Berber44/Controllers/IslemController.cs
+++ Berber44/Berber44/Controllers/IslemController.cs
@@ -37,9 +37,6 @@ namespace Berber44.Controllers
             return View();
         }
 
-
-
-
         // POST: Islem/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -51,6 +48,61 @@ namespace Berber44.Controllers
                 // Veritabanına ekle ve kaydet
                 _context.Islemler.Add(islem);
                 await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            // Eğer doğrulama başarısız olursa tekrar dropdown'ı yükle
+            ViewBag.Salonlar = new SelectList(_context.Salonlar, "Id", "Ad", islem.SalonId);
+            return View(islem);
+        }
+
+        // GET: Islem/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var islem = await _context.Islemler.FindAsync(id);
+            if (islem == null)
+            {
+                return NotFound();
+            }
+
+            // Dropdown için salon listesini yükle
+            ViewBag.Salonlar = new SelectList(_context.Salonlar, "Id", "Ad", islem.SalonId);
+            return View(islem);
+        }
+
+        // POST: Islem/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Islem islem)
+        {
+            if (id != islem.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(islem);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IslemExists(islem.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
 
@@ -93,5 +145,9 @@ namespace Berber44.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        private bool IslemExists(int id)
+        {
+            return _context.Islemler.Any(e => e.Id == id);
+        }
     }
 }
